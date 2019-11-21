3139d77d6932b665110b07d02b652e8b6992fdf1
Ernesto Medina <ernesto.m.medina.m@gmail.com>
2019-11-21 23:57:05 +0000

    end of class, seguimos ma;ana.

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -3,22 +3,42 @@ import React from "react";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
-export function Home() {
-	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+class Home extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			tasks: ["Primera tarea", "Segunda tarea", "Tercera tarea"],
+			newTask: ""
+		};
+	}
+	render() {
+		let tasks = this.state.tasks;
+		return (
+			<div className="container text-center">
+				<header className="todo-header">
+					<h1 className="display-3">Mi lista de tareas</h1>
+				</header>
+				<section className="todo-body">
+					<form>
+						<input placeholder="tareas nuevas!" />
+					</form>
+					<ul className="main-list mx-auto">
+						{tasks.map((value, index) => {
+							return (
+								<li key={index} className="list-item">
+									{index}
+								</li>
+							);
+						})}
+					</ul>
+				</section>
+				<footer className="list-footer">
+					<p>faltan x tareas por hacer</p>
+				</footer>
+			</div>
+		);
+	}
 }
+
+//create your first component
+export default Home;
--- src/js/index.js
+++ src/js/index.js
@@ -9,7 +9,7 @@ import "bootstrap";
 import "../styles/index.scss";
 
 //import your own components
-import { Home } from "./component/home.js";
+import Home from "./component/home.js";
 
 //render your react application
 ReactDOM.render(<Home />, document.querySelector("#app"));
