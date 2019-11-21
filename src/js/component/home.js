1497f1202e333daf8b0e988834afc1b1242d9d95
Ernesto Medina <ernesto.m.medina.m@gmail.com>
2019-11-21 22:31:40 +0000

    boilerplate listo.

---
+++ src/js/component/home.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+//include images into your bundle
+import rigoImage from "../../img/rigo-baby.jpg";
+
+//create your first component
+export function Home() {
+	return (
+		<div className="text-center mt-5">
+			<h1>Hello Rigo!</h1>
+			<p>
+				<img src={rigoImage} />
+			</p>
+			<a href="#" className="btn btn-success">
+				If you see this green button... bootstrap is working
+			</a>
+			<p>
+				Made by{" "}
+				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
+				love!
+			</p>
+		</div>
+	);
+}
