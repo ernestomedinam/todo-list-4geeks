e10acee5ee42e893c945872a813c7b9bb3612472
Ernesto Medina <ernesto.m.medina.m@gmail.com>
2019-11-22 01:27:12 +0000

    estilos corregidos.

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -7,34 +7,44 @@ class Home extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			tasks: ["Primera tarea", "Segunda tarea", "Tercera tarea"],
+			tasks: [
+				"Hacer café",
+				"Hacer la cama",
+				"Tomar agua",
+				"Regar las plantas"
+			],
 			newTask: ""
 		};
 	}
 	render() {
 		let tasks = this.state.tasks;
 		return (
-			<div className="container text-center">
-				<header className="todo-header">
+			<div className="container d-flex flex-column">
+				<header className="todo-header text-center mb-3">
 					<h1 className="display-3">Mi lista de tareas</h1>
 				</header>
 				<section className="todo-body">
-					<form>
-						<input placeholder="tareas nuevas!" />
+					<form className="text-center">
+						<input
+							className="my-5 mx-auto display-4"
+							placeholder="Agrega más tareas!"
+						/>
 					</form>
 					<ul className="main-list mx-auto">
 						{tasks.map((value, index) => {
 							return (
-								<li key={index} className="list-item">
-									{index}
+								<li
+									key={index}
+									className="list-item display-4 my-2 mx-0">
+									{value}
 								</li>
 							);
 						})}
 					</ul>
+					<footer className="list-footer mx-auto mt-5">
+						<p>faltan x tareas por hacer</p>
+					</footer>
 				</section>
-				<footer className="list-footer">
-					<p>faltan x tareas por hacer</p>
-				</footer>
 			</div>
 		);
 	}
