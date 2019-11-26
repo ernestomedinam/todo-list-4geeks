f1375a5a2f45ca155a4a01abe0fe3b1b0e4681d5
Ernesto Medina <ernesto.m.medina.m@gmail.com>
2019-11-26 18:24:13 -0400

    empezando clase 2.

--- src/js/index.js
+++ src/js/index.js
@@ -12,4 +12,4 @@ import "../styles/index.scss";
 import Home from "./component/home.js";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<Home />, document.querySelector("#root"));
