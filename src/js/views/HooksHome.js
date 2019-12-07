10e0342d60c7afd8fe26bf09e3f979cb4a10c302
Ernesto Medina <ernesto.m.medina.m@gmail.com>
2019-12-07 15:57:26 -0400

    hooksHome completed, list working with class context-flux; now to build hook based context-reducer.

--- src/js/views/HooksHome.js
+++ src/js/views/HooksHome.js
@@ -1,48 +1,132 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { AppContext } from "../store/appContext";
+import ListInput from "../component/ListInput";
+import ListItem from "../component/ListItem";
 
 const HooksHome = props => {
 	const { store, actions } = useContext(AppContext);
-	const [initLoad, setInitLoad] = useState(false);
-	const [isReady, setIsReady] = useState(false);
-	// mount & dismount effect
+	const [newTask, setNewTask] = useState("");
+	const [working, setWorking] = useState(true);
+	// working mount effect
 	useEffect(() => {
-		//effects
-		console.log("running on mount dismount effect");
-		if (!initLoad) {
-			setInitLoad(true);
-			setIsReady(true);
+		// effects
+		if (working) {
+			setWorking(false);
 		}
 		return () => {
-			//cleanup
+			// cleanup
 		};
 	}, []);
+	const handleAddTask = async e => {
+		e.preventDefault();
+		setWorking(true);
+		if (newTask.trim().length < 3) {
+			console.log("sorry, can't create such short tasks!");
+			alert("sorry, can't create such short tasks!");
+		} else {
+			let tasks = [
+				...store.tasks,
+				{
+					label: newTask,
+					done: false
+				}
+			];
+			let wasUpdated = await actions.fetchUpdateTasks(tasks);
+			if (wasUpdated) {
+				console.log("task added.");
+				setNewTask("");
+			} else {
+				console.log("could not add task, try again...");
+			}
+		}
+		setWorking(false);
+	};
+	const handleInputChange = e => {
+		setNewTask(e.target.value);
+	};
+	const disableInput = () => {
+		if (!store.tasks.length > 0 || working) {
+			return true;
+		} else {
+			return false;
+		}
+	};
+	const handleDeleteTask = async (e, idToDelete) => {
+		setWorking(true);
+		let tasksLeft = store.tasks.filter(
+			(task, index) => index != idToDelete
+		);
+		let wasUpdated = await actions.fetchUpdateTasks(tasksLeft);
+		if (wasUpdated) {
+			console.log("task deleted.");
+			setWorking(false);
+		}
+	};
 	return (
 		<div className="container d-flex flex-column">
 			<header className="todo-header text-center mb-3">
 				<h1 className="display-3">Mi lista de tareas</h1>
 			</header>
-			<ul>
-				{store.tasks.map((task, index) => {
-					return <li key={index}>{task.label}</li>;
-				})}
-			</ul>
+			<section className="todo-body">
+				<ListInput
+					onChangeHandler={handleInputChange}
+					onSubmitHandler={handleAddTask}
+					value={newTask}
+					disableInput={disableInput()}
+					storeIsReady={!working}
+				/>
+				<ul className="main-list mx-auto">
+					{store.tasks.map((task, index) => {
+						return (
+							<ListItem
+								key={index}
+								label={task.label}
+								onClickHandler={e => handleDeleteTask(e, index)}
+							/>
+						);
+					})}
+				</ul>
+				<footer className="list-footer mx-auto mt-5">
+					<p>
+						{store.tasks.length > 1
+							? `Faltan ${store.tasks.length} tareas por hacer...`
+							: store.tasks.length == 1
+								? `Falta ${
+										store.tasks.length
+								  } sola tarea por hacer!`
+								: "Felicitaciones, ya hiciste todo!!! Viva el ocio!!!"}
+					</p>
+				</footer>
+			</section>
 			<button
+				onClick={async () => {
+					setWorking(true);
+					await actions.fetchDeleteUser();
+					setWorking(false);
+				}}
 				className={
-					store.tasks.length > 0 && isReady
+					store.tasks.length > 0 && !working
 						? "btn btn-danger mt-4 mx-auto w-50"
 						: "btn btn-danger mt-4 mx-auto w-50 disabled"
 				}>
 				Borrar tareas y usuario
 			</button>
 			<button
+				onClick={async () => {
+					setWorking(true);
+					await actions.fetchCreateUser();
+					setWorking(false);
+				}}
 				className={
-					store.tasks.length > 0 || !isReady
+					store.tasks.length > 0 || working
 						? "btn btn-success my-2 mx-auto w-50 disabled"
 						: "btn btn-success my-2 mx-auto w-50"
 				}>
 				Crear usuario
 			</button>
+			<header className="todo-header text-center mb-3">
+				<h2 className="display-4">Otro contenido</h2>
+			</header>
 		</div>
 	);
 };
