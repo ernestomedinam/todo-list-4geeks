b78bd326e34c7c84aab45079796251e1d5af6559
Ernesto Medina <ernesto.m.medina.m@gmail.com>
2020-09-30 19:16:44 +0000

    adjusted for cloudinary migration regarding user_images

--- src/js/component/ImageCard.js
+++ src/js/component/ImageCard.js
@@ -9,7 +9,7 @@ const ImageCard = ({ image, url, onDelete }) => {
 				<span className="delete-image-button ml-4" onClick={onDelete} />
 			</div>
 			<div className="image-card-img-box">
-				<img src={url + "/" + image.image_url} />
+				<img src={image.image_url} />
 			</div>
 		</div>
 	);
